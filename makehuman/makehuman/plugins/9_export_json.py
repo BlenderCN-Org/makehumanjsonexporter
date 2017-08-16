061c9d3ea04f0a1f5f0ccb4132392f87e931ba88
Vrushali Prasade <vrushali@absentiavr.com>
2017-08-16 23:10:05 +0530

    added files

---
+++ makehuman/makehuman/plugins/9_export_json.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+"""
+**Project Name:**      MakeHuman
+
+**Product Home Page:** http://www.makehuman.org/
+
+**Code Home Page:**    https://bitbucket.org/MakeHuman/makehuman/
+
+**Authors:**           Daryl Anderman
+
+**Copyright(c):**      MakeHuman Team 2001-2017
+
+**Licensing:**         AGPL3
+
+    This file is part of MakeHuman (www.makehuman.org).
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+Abstract
+--------
+
+JSON design optionsexporter
+"""
+
+import gui
+from export import Exporter
+
+class ExporterJSON(Exporter):
+    def __init__(self):
+        Exporter.__init__(self)
+        self.group = "json"
+        self.name = "Design Options"
+        self.filter = "JSON (*.json)"
+
+    def build(self, options, taskview):
+        pass
+
+    def export(self, human, filename):
+        import loaddesign
+ 
+        filepath = filename("json")
+        loaddesign.savevalues(filepath)
+
+    def onShow(self, exportTaskView):
+        exportTaskView.scaleBox.hide()
+
+    def onHide(self, exportTaskView):
+        exportTaskView.scaleBox.show()
+
+
+def load(app):
+    app.addExporter(ExporterJSON())
+
+def unload(app):
+    pass
